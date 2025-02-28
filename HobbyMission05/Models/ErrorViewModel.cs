54845806fcde99469afbf9a8e6b3f28f3412f8b0
azorrilla181 <azorrilla181@gmail.com>
2025-02-28 16:02:41 -0700

    Add project files.

---
+++ HobbyMission05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HobbyMission05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
